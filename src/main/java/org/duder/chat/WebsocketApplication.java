15fd819435fbb65f944e72622099aea2a5d207a7
Kubieniec <krzysztof.kubieniec@hotmail.com>
2020-03-22 15:35:28 +0100

    changed main class name

--- src/main/java/org/duder/chat/WebsocketApplication.java
+++ src/main/java/org/duder/chat/WebsocketApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebsocketDemoApplication {
+public class WebsocketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebsocketDemoApplication.class, args);
+		SpringApplication.run(WebsocketApplication.class, args);
 	}
 }
