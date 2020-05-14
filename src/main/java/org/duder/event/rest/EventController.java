eab518d5ff8e8ad9ff220e35e321dc3935c9c265
Krzysztof Kubieniec <kkubieniec@amelco.co.uk>
2020-05-14 23:41:45 +0200

    changed create event endpoint to multipart

--- src/main/java/org/duder/event/rest/EventController.java
+++ src/main/java/org/duder/event/rest/EventController.java
@@ -19,7 +19,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -47,8 +49,15 @@ class EventController {
     }
 
     @PostMapping()
-    public ResponseEntity<Void> create(@RequestBody String createEvent, @RequestHeader("Authorization") String sessionToken) throws JsonProcessingException {
+    public ResponseEntity<Void> create(
+            @RequestPart String createEvent,
+            @RequestPart(value="image", required = false) final MultipartFile image,
+            @RequestHeader("Authorization") String sessionToken
+    ) throws JsonProcessingException {
         logger.info("Received create event request " + createEvent + " with sessionToken = " + sessionToken);
+        if (image != null) {
+            logger.info("Received create event image " + image);
+        }
         Long eventId = eventService.create(new ObjectMapper().readValue(createEvent, CreateEvent.class), sessionToken);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest().path("/{id}")
