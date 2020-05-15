c9a5830994cfeb32812711b5ba8e6376d89676a0
Krzysztof Kubieniec <kkubieniec@amelco.co.uk>
2020-05-15 21:00:58 +0200

    get image test2

--- src/main/java/org/duder/common/ImageController.java
+++ src/main/java/org/duder/common/ImageController.java
@@ -1,30 +1,38 @@
 package org.duder.common;
 
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.support.ServletContextResource;
-import org.apache.commons.io.IOUtils;
 
-import java.io.File;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @RestController
-public class ImageController {
+public class ImageController extends DuderBean{
 
         @GetMapping(
                 value = "/get-image-with-media-type",
                 produces = MediaType.IMAGE_JPEG_VALUE
         )
-        public @ResponseBody
-        byte[] getImageWithMediaType() throws IOException {
-            return Files.readAllBytes(Paths.get("images/pexels-photo-1040626.jpeg"));
+        @ResponseBody
+        public HttpEntity<Void> getImageWithMediaType(HttpServletResponse response) throws IOException {
+                byte[] bytes = Files.readAllBytes(Paths.get("images/pexels-photo-1040626.jpeg"));
+                try (InputStream inputStream = new ByteArrayInputStream(bytes)) {
+                        StreamUtils.copy(inputStream, response.getOutputStream());
+                        response.setContentType(MediaType.IMAGE_PNG_VALUE);
+                } catch (IOException e) {
+                    error("Error during coping image stream to http response", e);
+                }
+                return new ResponseEntity(HttpStatus.OK);
         }
 
 }
--- src/main/java/org/duder/common/ImageService.java
+++ src/main/java/org/duder/common/ImageService.java
@@ -1,8 +1,5 @@
 package org.duder.common;
 
-import org.duder.dto.user.Dude;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,16 +10,16 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Component
 public class ImageService extends DuderBean {
 
     public static final String imagesDir = "images/";
 
-    @PostConstruct
+    public ImageService() {
+        makeDirIfNotExists();
+    }
+
     private void makeDirIfNotExists() {
         File directory = new File(imagesDir);
         if (!directory.exists()) {
