e426bb80bf8b639e4795322805cdc7521633b91e
Krzysztof Kubieniec <kkubieniec@amelco.co.uk>
2020-06-21 10:33:28 +0200

    dynamicInsert instead of Entity in FriendInvitation

--- src/main/java/org/duder/user/model/FriendInvitation.java
+++ src/main/java/org/duder/user/model/FriendInvitation.java
@@ -5,9 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -16,7 +16,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-@Entity
+@DynamicInsert
 @Getter
 @Setter
 @Builder
